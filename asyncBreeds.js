1eb6460b39dbf18b6e74f2d644338d42b8849584
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-28 22:54:51 +0000

    Testing Asynchronous Return Values stretch exercise - developing and debugging error conditions and tests for async callbacks

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,21 +2,27 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, whenDone) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+  // console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
+    // console.log("In readFile's Callback: it has the data.");
     // Data must be passed to callback, instead of being directly returned
-    if (!error) whenDone(data);
+    if (!error) {
+      whenDone(data);
+    } else {
+      whenDone(undefined);
+    }
   });
 };
 
 // console.log become a function
 
-const printCatBreed = breed => {
-  console.log('Return Value: ', breed);
-}
+// const printCatBreed = breed => {
+// console.log('Return Value: ', breed);
+// }
 
 // function now has two arguments: breed string and callback function
 
-breedDetailsFromFile('Bombay', printCatBreed);
-breedDetailsFromFile('Balinese', printCatBreed);
+// breedDetailsFromFile('Bombay', printCatBreed);
+// breedDetailsFromFile('Balinese', printCatBreed);
+
+module.exports = breedDetailsFromFile;
