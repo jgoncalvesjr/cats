2cf79399cfc105619a66d90ab90e8b0fe09baa37
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-07-28 20:24:38 +0000

    Asynchronous Return Values exercise

---
+++ asyncBreeds.js
@@ -0,0 +1,22 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, whenDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    // Data must be passed to callback, instead of being directly returned
+    if (!error) whenDone(data);
+  });
+};
+
+// console.log become a function
+
+const printCatBreed = breed => {
+  console.log('Return Value: ', breed);
+}
+
+// function now has two arguments: breed string and callback function
+
+breedDetailsFromFile('Bombay', printCatBreed);
+breedDetailsFromFile('Balinese', printCatBreed);
